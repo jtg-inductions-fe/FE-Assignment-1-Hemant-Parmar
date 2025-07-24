3f30ee1a989050287b41739aac74410057413217
hemant-parmar-jtg <hemant.parmar@joshtechnologygroup.com>
2025-07-25 00:51:33 +0530

    HP_RU_01: create header with responsiveness

--- src/script/header.js
+++ src/script/header.js
@@ -7,8 +7,6 @@ menuBtn.addEventListener('click', () => {
     menuBtn.setAttribute('aria-expanded', String(!isOpen));
     nav.hidden = isOpen;
 
-    // Icon is changed here, for now.
-
     if (!isOpen) {
         trapFocus();
     }
@@ -40,3 +38,42 @@ document.addEventListener('keydown', (e) => {
         nav.hidden = true;
     }
 });
+
+// To put the Login/Signup buttons in btnContainer for tablet view
+
+const btnContainer = document.getElementById('tablet-btn-container');
+const primaryBtn = document.querySelector('.header__nav__btn.btn-primary');
+const secondaryBtn = document.querySelector('.header__nav__btn.btn-secondary');
+
+const navList = document.querySelector('.header__nav__list');
+
+function moveNavBtn() {
+    const width = window.innerWidth;
+
+    // Tablet view
+    if (width >= 1024 && width < 1440) {
+        btnContainer.removeAttribute('hidden');
+
+        if (!btnContainer.contains(secondaryBtn)) {
+            btnContainer.appendChild(secondaryBtn);
+        }
+        if (!btnContainer.contains(primaryBtn)) {
+            btnContainer.appendChild(primaryBtn);
+        }
+    }
+
+    // Mobile or Desktop view -> put them back in nav-list
+    else {
+        btnContainer.setAttribute('hidden', '');
+        if (!navList.contains(primaryBtn)) {
+            navList.appendChild(primaryBtn);
+        }
+        if (!navList.contains(secondaryBtn)) {
+            navList.appendChild(secondaryBtn);
+        }
+    }
+}
+
+// Run on load & resize
+window.addEventListener('resize', moveNavBtn);
+window.addEventListener('DOMContentLoaded', moveNavBtn);
