e722e4cbc3b129a43e401571f406ba6d027669f2
hemant-parmar-jtg <hemant.parmar@joshtechnologygroup.com>
2025-08-12 01:13:56 +0530

    HP_RU_01: Open the nav menu from right

--- src/script/index.js
+++ src/script/index.js
@@ -2,12 +2,8 @@ const menuBtn = document.getElementById('menu-btn');
 const nav = document.getElementById('nav');
 const navList = document.querySelector('.header__list');
 const navBtns = document.querySelector('.header__btn-container');
-
-// Early exit if required elements are missing
-if (!menuBtn || !nav || !navList || !navBtns) {
-    // eslint-disable-next-line no-console
-    console.warn('Required navigation elements not found');
-}
+const largeMediaQuery = window.matchMedia('(min-width: 1440px)'); // To check whether the device has large width (desktop-view)
+const mediumMediaQuery = window.matchMedia('(min-width: 1024px)'); // To check whether the device has medium width (tablet-view)
 
 const focusable = nav.querySelectorAll(
     'a, button, [tabindex]:not([tabindex="-1"])',
@@ -31,20 +27,37 @@ const trapFocus = (e) => {
     }
 };
 
+// To make nav elements inert according to the screen sizes
+const addResponsiveInert = () => {
+    if (largeMediaQuery.matches) return;
+    else if (mediumMediaQuery) navList.inert = true;
+    else nav.inert = true;
+};
+
+// To remove inert from nav elements
+const removeInert = () => {
+    navList.inert = false;
+    nav.inert = false;
+};
+
 const openMenu = () => {
+    menuBtn.setAttribute('aria-expanded', 'true');
     nav.classList.add('header__nav--open');
     navList.classList.remove('header__list--hidden');
     navBtns.classList.remove('header__btn-container--hidden');
     document.addEventListener('keydown', trapFocus);
-    document.querySelector('main').setAttribute('inert', '');
+    document.querySelector('main').inert = true;
+    removeInert();
 };
 
 const closeMenu = () => {
+    addResponsiveInert();
+    document.querySelector('main').inert = false;
     document.removeEventListener('keydown', trapFocus);
-    navList.classList.add('header__list--hidden');
     navBtns.classList.add('header__btn-container--hidden');
+    navList.classList.add('header__list--hidden');
     nav.classList.remove('header__nav--open');
-    document.querySelector('main').removeAttribute('inert');
+    menuBtn.setAttribute('aria-expanded', 'false');
 };
 
 menuBtn.addEventListener('click', () => {
@@ -55,8 +68,6 @@ menuBtn.addEventListener('click', () => {
     } else {
         closeMenu();
     }
-
-    menuBtn.setAttribute('aria-expanded', String(!isOpen));
 });
 
 document.addEventListener('keydown', (e) => {
@@ -65,8 +76,10 @@ document.addEventListener('keydown', (e) => {
         menuBtn.getAttribute('aria-expanded') === 'true'
     ) {
         closeMenu();
-        menuBtn.setAttribute('aria-expanded', 'false');
     }
 });
+
+largeMediaQuery.addEventListener('change', closeMenu);
+mediumMediaQuery.addEventListener('change', closeMenu);
 
 closeMenu();
